6b803a7009f5c28bd1a81e86b16b29966c6258db
Josemar Sabino <josemarsabino@hotmail.com>
2021-07-09 12:58:51 -0300

    Configuração Projeto Inicial e funcionando

---
+++ src/main/java/br/com/sada/SpringBootFirebirdApplication.java
@@ -0,0 +1,13 @@
+package br.com.sada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirebirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirebirdApplication.class, args);
+	}
+
+}
